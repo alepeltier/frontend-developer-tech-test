e5ebf3075ea38f58801aa0d18b7850a5ada0f8ba
Marcel <71490170+mnblski@users.noreply.github.com>
2024-01-31 21:39:14 +0000

    feat: fetch titles on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import WidthContainer from "@/components/layout/width-container/WidthContainer";
+import titleServices from "@/services/titleServices";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const page = new URLSearchParams(searchParams).get("page");
+
+  const data = await titleServices.getManyTitles({
+    page: page ? parseInt(page) : 1,
+    list: "top_boxoffice_200",
+    limit: 20,
+  });
+
+  console.log("data", data);
+
   return (
     <div className="bg-amber-500">
       <WidthContainer>
