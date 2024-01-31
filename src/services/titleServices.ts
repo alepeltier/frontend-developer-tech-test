234c5c23e110aec4a99dea18872ba52f623f6968
Marcel <71490170+mnblski@users.noreply.github.com>
2024-01-31 21:38:30 +0000

    feat: add title services

---
+++ src/services/titleServices.ts
@@ -0,0 +1,65 @@
+import axios from "axios";
+
+// const RAPID_API_KEY = "f3ebaab43amsh5091701e976d199p11ddcdjsna8253cfa57eb";
+// const RAPID_API_HOST = "moviesdatabase.p.rapidapi.com";
+// const RAPID_API_BASE_URL = "https://moviesdatabase.p.rapidapi.com";
+
+const RAPID_BASE_URL = process.env.RAPID_API_BASE_URL;
+
+const options = {
+  headers: {
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+    "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+  },
+};
+
+export interface GetManyTitlesResponse<T = any> {
+  page: string;
+  next: string | null;
+  entries: number;
+  results: T[] | null;
+}
+
+export interface GetSingleTitleResponse<T = any> {
+  results: T | null;
+}
+
+// TODO: write proper types & type guards
+export interface GetManyTitlesParams {
+  list?: string;
+  limit?: number;
+  page?: number;
+  info?: string;
+}
+
+interface GetSingleTitleParams {
+  id: string;
+  info?: string; // provide pre-defined list
+}
+
+export default {
+  getManyTitles: async (
+    params?: GetManyTitlesParams
+  ): Promise<GetManyTitlesResponse> => {
+    const getManyTitlesUrl = `${RAPID_BASE_URL}/titles`;
+    const response = await axios.get<GetManyTitlesResponse>(getManyTitlesUrl, {
+      ...options,
+      params,
+    });
+    return response.data;
+  },
+  getSingleTitle: async (
+    id: string,
+    params?: GetSingleTitleParams
+  ): Promise<GetSingleTitleResponse> => {
+    const getSingleTitleUrl = `${RAPID_BASE_URL}/titles/${id}`;
+    const response = await axios.get<GetSingleTitleResponse>(
+      getSingleTitleUrl,
+      {
+        ...options,
+        params,
+      }
+    );
+    return response.data;
+  },
+};
