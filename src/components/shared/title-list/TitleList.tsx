2c51bc7858d3efd226060c3202db0d9abcfe616d
Marcel <71490170+mnblski@users.noreply.github.com>
2024-01-31 21:59:14 +0000

    feat: add title list pagination

--- src/components/shared/title-list/TitleList.tsx
+++ src/components/shared/title-list/TitleList.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button/Button";
 import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import { GetManyTitlesResponse } from "@/services/titleServices";
 import { TitleCard, TitleImage, TitleCardInfo } from "../title-card/TitleCard";
 
@@ -9,6 +13,47 @@ export interface TitleListPaginationProps
   next: string | null;
   page: string;
 }
+
+const TitleListPagination = ({
+  next,
+  page,
+  className,
+}: TitleListPaginationProps) => {
+  const pathname = usePathname();
+
+  return (
+    <div className={cn("w-full flex gap-1 justify-end", className)}>
+      <Button
+        size="sm"
+        variant="outline"
+        disabled={!page || page.toString() === "1"}
+        asChild
+      >
+        <Link
+          href={`${pathname}?page=${parseInt(page) - 1}`}
+          className={cn({
+            "opacity-40 pointer-events-none": !page || page?.toString() === "1",
+          })}
+        >
+          <ChevronLeft className="h-4 w-4" />
+          Previous
+        </Link>
+      </Button>
+      <Button size="sm" variant="outline" disabled={!next} asChild>
+        <Link
+          href={`${pathname}?page=${parseInt(page) + 1}`}
+          className={cn({
+            "opacity-40 pointer-events-none": !next,
+          })}
+        >
+          Next
+          <ChevronRight className="h-4 w-4" />
+        </Link>
+      </Button>
+    </div>
+  );
+};
+TitleListPagination.displayName = "TitleListPagination";
 
 export interface TitleListProps extends React.HTMLAttributes<HTMLDivElement> {
   initialData: GetManyTitlesResponse; // improve types??
@@ -25,6 +70,7 @@ const TitleList = ({ initialData, className }: TitleListProps) => {
 
   return (
     <div className={cn("flex flex-col gap-4", className)}>
+      <TitleListPagination next={initialData.next} page={initialData.page} />
       <div className="grid grid-cols-2 3 gap-3 md:grid-cols-3 lg:grid-cols-5">
         {initialData.results.map((title) => (
           <TitleCard key={title.id} href={`/titles/${title.id}`}>
@@ -39,9 +85,10 @@ const TitleList = ({ initialData, className }: TitleListProps) => {
           </TitleCard>
         ))}
       </div>
+      <TitleListPagination next={initialData.next} page={initialData.page} />
     </div>
   );
 };
 TitleList.displayName = "TitleList";
 
-export { TitleList };
+export { TitleList, TitleListPagination };
