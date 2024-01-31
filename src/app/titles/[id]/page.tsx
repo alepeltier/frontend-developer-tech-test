178d1ecbd8b3fa86fd86940cd4451ee7e2b022b0
Marcel <71490170+mnblski@users.noreply.github.com>
2024-01-31 21:38:44 +0000

    feat: titles/[id] page

---
+++ src/app/titles/[id]/page.tsx
@@ -0,0 +1,11 @@
+import WidthContainer from "@/components/layout/width-container/WidthContainer";
+
+export default function TitlePage() {
+  return (
+    <div>
+      <WidthContainer>
+        <p>Title Page</p>
+      </WidthContainer>
+    </div>
+  );
+}
