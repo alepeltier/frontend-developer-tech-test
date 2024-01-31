c27b858a9b7e0bd09b51894219eb9186ad0c4960
Marcel <71490170+mnblski@users.noreply.github.com>
2024-01-31 22:13:11 +0000

    feat: add title page components

---
+++ src/app/titles/[id]/_components/TitleBaseInfo.tsx
@@ -0,0 +1,86 @@
+import { Badge } from "@/components/ui/badge/Badge";
+import { secondsToMin } from "@/lib/utils";
+
+//TODO breakdown into components
+// TODO fix types
+interface TitleInfoProps<T = any> {
+  titleData: T;
+}
+
+export default function TitleBaseInfo({ titleData }: TitleInfoProps) {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold mb-6">
+        {titleData.originalTitleText.text}
+      </h1>
+      <div className="md:border md:p-6 md:rounded-2xl md:h-fit md:border">
+        <div className="grid gap-4">
+          <div className="flex gap-4 items-center justify-between">
+            <span className="text-sm text-muted-foreground font-semibold">
+              Genre
+            </span>
+            <span className="text-sm font-semibold">
+              {titleData.genres.genres[0].text}
+            </span>
+          </div>
+
+          <div className="flex gap-4 items-center justify-between">
+            <span className="text-sm text-muted-foreground font-semibold">
+              Rating
+            </span>
+            <span className="text-sm font-semibold">
+              {titleData?.ratingsSummary.aggregateRating}/10
+            </span>
+          </div>
+
+          <div className="flex gap-4 items-center justify-between">
+            <span className="text-sm text-muted-foreground font-semibold">
+              Release Year
+            </span>
+            <span className="text-sm font-semibold">
+              {titleData.releaseYear.year}
+            </span>
+          </div>
+          <div className="flex gap-4 items-center justify-between">
+            <span className="text-sm text-muted-foreground font-semibold">
+              Runtime{" "}
+            </span>
+            <span className="text-sm font-semibold">
+              {secondsToMin(titleData.runtime?.seconds || 0)}min
+            </span>
+          </div>
+
+          {titleData.meterRanking?.currentRank ? (
+            <div className="flex gap-4 items-center justify-between">
+              <span className="text-sm text-muted-foreground font-semibold">
+                Popularity{" "}
+              </span>
+              <span className="text-sm font-semibold">
+                {titleData.meterRanking?.currentRank}
+              </span>
+            </div>
+          ) : null}
+          <hr className="h-px my-4 flex-1 bg-border"></hr>
+          <div className="">
+            <span className="block text-muted-foreground font-semibold mb-3">
+              All Genres
+            </span>
+            <div className="flex flex-wrap gap-2">
+              {titleData.genres.genres.map(
+                (genre: TitleGenre, index: number) => (
+                  <Badge
+                    variant="secondary"
+                    className="py-2 px-4"
+                    key={`${genre.id}-${index}`}
+                  >
+                    {genre.text}
+                  </Badge>
+                )
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/titles/[id]/_components/TitleMoreInfo.tsx
@@ -0,0 +1,21 @@
+interface TitleMoreInfoProps<T = any> {
+  titleData: T;
+}
+
+// TODO breakdown into components
+
+export default function TitleMoreInfo({ titleData }: TitleMoreInfoProps) {
+  return (
+    <div className="grid gap-6 mt-10 md:grid-cols-[1.5fr,1fr] md:mt-20 xl:grid-cols-[2fr,1fr]">
+      <div>
+        <h2 className="text-lg font-semibold mb-4">Plot summary</h2>
+        <p className="text-muted-foreground">
+          {titleData.plot?.plotText?.plainText}
+        </p>
+      </div>
+      {/* <div>
+          <h3 className="text-lg font-semibold mb-4">Crew</h3>
+          </div> */}
+    </div>
+  );
+}
